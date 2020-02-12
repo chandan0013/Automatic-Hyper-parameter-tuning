b76921442678b2abff6d890de731262a3509c6eb
Chandan Dhal <chandandhal.er@gmail.com>
2020-02-12 11:12:23 -0800

    Add files via upload

---
+++ BO_optimization.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+fname = ('C:/Users/Chandan/Documents/CPTS 570/Homework/HW 2/data/fashion-mnist_train.csv')
+data_train = np.loadtxt(fname, delimiter = ',', skiprows = 1)
+
+X= data_train[0:5000,1:]
+
+y = data_train[0:5000,0]
+
+
+n_features = X.shape[1]
+
+# gradient boosted trees tend to do well on problems like this
+reg = GradientBoostingRegressor(n_estimators=50, random_state=0)
+reg
+
+
+# The list of hyper-parameters we want to optimize. For each one we define the bounds,
+# the corresponding scikit-learn parameter name, as well as how to sample values
+# from that dimension (`'log-uniform'` for the learning rate)
+space  = [Integer(1, 50, name='max_depth'),
+          Real(10**-4, 10**3, "log-uniform", name='learning_rate'),
+          Integer(1, n_features, name='max_features'),
+          Integer(2, 100, name='min_samples_split'),
+          Integer(1, 100, name='min_samples_leaf')]
+
+# this decorator allows your objective function to receive a the parameters as
+# keyword arguments. This is particularly convenient when you want to set scikit-learn
+# estimator parameters
+@use_named_args(space)
+def objective(**params):
+    reg.set_params(**params)
+
+    return -np.mean(cross_val_score(reg, X, y, cv=5, n_jobs=-1,
+                                    scoring="neg_mean_absolute_error"))
+
+
+# In[ ]:
+
+
+
+res_gp = gp_minimize(objective, space, n_calls=50, random_state=0)
+
+"Best score=%.4f" % res_gp.fun
+
+
+# In[ ]:
+
+
+print("""Best parameters:
+- max_depth=%d
+- learning_rate=%.6f
+- max_features=%d
+- min_samples_split=%d
+- min_samples_leaf=%d""" % (res_gp.x[0], res_gp.x[1], 
+                            res_gp.x[2], res_gp.x[3], 
+                            res_gp.x[4]))
+
+
+# In[ ]:
+
+
+from skopt.plots import plot_convergence
+
+plot_convergence(res_gp)
+
